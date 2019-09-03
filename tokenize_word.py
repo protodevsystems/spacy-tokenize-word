75a5edd8866985821efea996477140771e0354c5
Alex Lim <protodevsystems@gmail.com>
2019-09-03 22:33:19 +0800

    Initial commit

---
+++ tokenize_word.py
@@ -0,0 +1,55 @@
+# Run this to load en_core_web_sm into your spacy module
+# $ python -m spacy download en_core_web_sm
+
+# Legend for token.dep_:
+# CC	coordinating conjunction
+# CD	cardinal digit
+# DT	determiner
+# EX	existential there (like: "there is" ... think of it like "there exists")
+# FW	foreign word
+# IN	preposition/subordinating conjunction
+# JJ	adjective	'big'
+# JJR	adjective, comparative	'bigger'
+# JJS	adjective, superlative	'biggest'
+# LS	list marker	1)
+# MD	modal	could, will
+# NN	noun, singular 'desk'
+# NNS	noun plural	'desks'
+# NNP	proper noun, singular	'Harrison'
+# NNPS	proper noun, plural	'Americans'
+# PDT	predeterminer	'all the kids'
+# POS	possessive ending	parent's
+# PRP	personal pronoun	I, he, she
+# PRP$	possessive pronoun	my, his, hers
+# RB	adverb	very, silently,
+# RBR	adverb, comparative	better
+# RBS	adverb, superlative	best
+# RP	particle	give up
+# TO	to	go 'to' the store.
+# UH	interjection	errrrrrrrm
+# VB	verb, base form	take
+# VBD	verb, past tense	took
+# VBG	verb, gerund/present participle	taking
+# VBN	verb, past participle	taken
+# VBP	verb, sing. present, non-3d	take
+# VBZ	verb, 3rd person sing. present	takes
+# WDT	wh-determiner	which
+# WP	wh-pronoun	who, what
+# WP$	possessive wh-pronoun	whose
+# WRB	wh-abverb	where, when
+
+import spacy
+nlp = spacy.load('en_core_web_sm')
+
+while True:
+    sample_input = input('Enter sentence:')
+    doc = nlp(sample_input)
+
+    for token in doc:
+        print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
+                token.shape_, token.is_alpha, token.is_stop)
+
+
+
+
+
